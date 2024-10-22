8f43e5d6492377d2def89be37b2113dcf5de9bac
Thomas Wissemann <tcwissemann@gmail.com>
2024-10-22 01:37:09 -0500

    small updates
    source\templates is a small holdover until AllAuth

--- source/config/settings.py
+++ source/config/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
     'core.apps.CoreConfig',
     'dash.apps.DashConfig',
 ]
